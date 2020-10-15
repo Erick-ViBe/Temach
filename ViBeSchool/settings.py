cd855273d89809dbce6baf60419d6437986f8cf3
Erick-ViBe <50604053+Erick-ViBe@users.noreply.github.com>
2020-10-15 16:15:00 -0500

    Update settings.py

--- ViBeSchool/settings.py
+++ ViBeSchool/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1uylm0d7%5+006a=fdt&s!98-z(#ken-i#kg)tgxoale3*+h=*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['temach.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['temach.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
